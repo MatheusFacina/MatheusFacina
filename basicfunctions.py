11d911ca7e2131289de2c73353409839a8553cd7
MatheusFacina <matheusfacinadavid@gmail.com>
2022-04-17 13:37:09 -0300

    Create basicfunctions.py

---
+++ basicfunctions.py
@@ -0,0 +1,18 @@
+#função pra colocar em um while loop perguntando alguma coisa e se deseja continuar
+def ask_ok(prompt, follow="Continando "):
+    while True:
+            ok = input(prompt)
+            if ok in ["SIM", "SI", "S", "sim", "si", "s", "ok"]:
+                print(follow)
+                return True
+            elif ok in ["n", "na", "nao", "não", "nã" "N", "NA", "NÃ", "NÃO", "NAO"]:
+                print("\nEntão digite novamente ")
+                return False
+            else:
+                print("\nPOR FAVOR DIGITE SIM OU NAO")
+           
+if __name__ == "__main__":
+    whilhe True:
+        dataNasc = input("Data de nascimento com barras(ex. 01/01/1900): ")
+        if ask_ok("A data " + '\033[1m' + dataNasc + "\033[0;0m" +" está correta? ", "Continuando a data " + '\033[1m' + dataNasc + "\033[0;0m")==True:
+            break
